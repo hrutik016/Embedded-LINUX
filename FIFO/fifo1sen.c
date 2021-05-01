8d396ea6200d62ef2cbe9ccde8775214cb1c195d
hrutik <hrutik716@gmail.com>
2021-05-01 17:34:34 +0530

    FIFO added

---
+++ FIFO/fifo1sen.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main()
+{
+	int fd;
+	char * myfifo = "/tmp/myfifo";
+	/* create the FIFO (named pipe) */
+	mkfifo(myfifo, 0666);
+	/* write "Hi" to the FIFO */
+	fd=open(myfifo, O_WRONLY);
+	write(fd, "Hi", sizeof("Hi"));
+	close(fd);
+	/* remove the FIFO */
+	unlink(myfifo);
+	return 0;	
+}
+
